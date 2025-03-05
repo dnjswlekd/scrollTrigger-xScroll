f803361cdb7f1579442f6312c5dca05f73166489
dnjswlekd <wj8014@gmail.com>
2025-03-05 20:52:59 +0900

    clip path 를 활용한 스크롤에 따라 이미지 사이즈 변경하기

--- js/script.js
+++ js/script.js
@@ -21,8 +21,49 @@ $(function () {
           // viewport위치의 300% => 클수록 느려짐
           pin: true,
           scrub: 1,
-          markers: true,
+          markers: false,
         },
+      });
+
+      // img motion
+      gsap.utils.toArray('.img-box').forEach(function (imgBox) {
+        // 이미지 박스가 커지는 애니메이션 => 화면 오른쪽에서 부터 화면 중앙에서 끝남
+        gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: imgBox,
+              containerAnimation: scrollTween,
+              start: 'center right',
+              end: 'center center',
+              scrub: true,
+              markers: false,
+            },
+          })
+
+          .to(
+            imgBox,
+            { 'clip-path': 'inset(0%', ease: 'none', duration: 1 },
+            0
+          );
+
+        // 이미지 박스가 작아지는 애니메이션 => 화면 중앙에서 부터 화면 왼쪽에서 끝남
+        gsap
+          .timeline({
+            scrollTrigger: {
+              trigger: imgBox,
+              containerAnimation: scrollTween,
+              start: 'center center',
+              end: 'center left',
+              scrub: true,
+              markers: false,
+            },
+          })
+
+          .to(
+            imgBox,
+            { 'clip-path': 'inset(30%', ease: 'none', duration: 1 },
+            0
+          );
       });
     },
   });
