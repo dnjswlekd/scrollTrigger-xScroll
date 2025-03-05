9bb1dcc283eb98c136969e6a288f78cc001d22f7
dnjswlekd <wj8014@gmail.com>
2025-03-05 20:08:56 +0900

    gsap matchmedia 로 가로스크롤 이벤트

--- js/script.js
+++ js/script.js
@@ -4,4 +4,26 @@ $(function () {
     mobile: true,
     once: false,
   });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  ScrollTrigger.matchMedia({
+    '(min-width: 1024px)': function () {
+      // 02 가로스크롤
+      let list = gsap.utils.toArray('.work ul li');
+      let scrollTween = gsap.to(list, {
+        xPercent: -100 * (list.length - 1),
+        ease: 'none',
+        scrollTrigger: {
+          trigger: '.work',
+          start: 'center center',
+          end: '300%',
+          // viewport위치의 300% => 클수록 느려짐
+          pin: true,
+          scrub: 1,
+          markers: true,
+        },
+      });
+    },
+  });
 });
